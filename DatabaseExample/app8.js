7c0157ec75d2773b59a655898c031c29da0ae3f2
JiHoon_KiM <59499356+JiHoon-JK@users.noreply.github.com>
2020-12-27 16:49:23 +0900

    20.12.27 챕터 6 : 데이터 베이스 사용하기(5)
    - mysql 로 node.js 연결해서 작성하기

--- DatabaseExample/app8.js
+++ DatabaseExample/app8.js
@@ -101,63 +101,74 @@ var addUser = function(id, name, age, password, callback){
 var authUser = function(database, id, password, callback){
     console.log('authUser 호출됨.');
     
-    // users 컬렉션 참조
-    var users = database.collection('users');
-    
-    // 아이디와 비밀번호를 사용해 검색
-    users.find({"id":id, "password":password}).toArray(function(err,docs){
-        if(err) {
+    // 커넥션 풀에서 연결 객체를 가져온다.
+    pool.getConnection(function(err,conn){
+        if(err){
+            if(conn){
+                conn.release(); // 반드시 해제해야한다.
+            }
             callback(err, null);
             return;
         }
-        // 검색한 결과가 있는 경우
-        if(docs.length > 0){
-            console.log('아이디 [%s]. 비밀번호 [%s]가 일치하는 사용자 찾음,',id,password);
-            callback(null,docs);
-        }
-        // 검색한 결과가 없는 경우
-        else{
-            console.log('일치하는 사용자를 찾지 못함.');
-            callback(null,null);
-        }
-    });
+        console.log('데이터베이스 연결 스레드 아이디 : ' + conn.threadId);
+        
+        var columns = ['id', 'name', 'age'];
+        var tablename = 'users';
+        
+        //SQL 문을 실행한다.
+        var exec = conn.query("select ?? from ?? where id = ? and password = ?", [columns, tablename, id, password], function(err,rows){
+            conn.release(); // 반드시 해제해야한다.
+            console.log('실행 대상 SQL : ' + exec.sql);
+            
+            if(rows.length > 0 ){
+                console.log('아이디 [%s], 패스워드 [%s] 가 일치하는 사용자 찾음', id, password);
+                callback(null,rows);
+            }else{
+                console.log('일치하는 사용자 찾지 못함');
+                callback(null,null);
+            }
+        });
+    })
 }
 
 // 로그인 라우터 함수 - 데이터베이스의 정보와 비교
 router.route('/process/login').post(function(req,res){
     console.log('/process/login 호출됨.');
     
-    var paramId = req.param('id');
-    var paramPassword = req.param('password');
+    // 요청 파라미터 확인
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
     
-    if(database){
-        authUser(database, paramId, paramPassword, function(err, docs){
-            if(err) {throw err;}
-            
-            if(docs){
-                console.dir(docs);
-                var username = docs[0].name;
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+    
+    //pool 객체가 초기화된 경우, authUser 함수 호출하여 사용자 인증
+    if(pool){
+        authUser(paramId, paramPassword, function(err,rows){
+            // 오류가 발생했을 때 클라이언트로 오류 전송
+            if(err){
+                console.error('사용자 로그인 중 오류 발생 : ' + err.stack);
+                
                 res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-                res.write('<h1>로그인 성공</h1>');
-                res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
-                res.write('<div><p>사용자 이름 : ' + paramPassword + '</p></div>');
-                res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+                res.write('<h2>사용자 로그인 중 오류 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
                 res.end();
-            }else{
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-                res.write('<h1>로그인 실패</h1>');
-                res.write('<div><p>아이디와 비밀번호를 다시 확인하십시오.</p></div>');
-                res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+                
+                return;
+            } 
+            if(rows){
+                console.dir(rows);
+                
+                var username = rows[0].name;
+                
+                res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
+                res.write('<h1>로그인 성공</h1>');
+                res.write('<div><p>사용자 아이디 : ' + paramId +  '</p></div>');
+                res.write('<div><p>사용자 이름' + username + '</p></div>');
+                res.write("<br><br><a href='/public/login2.html'>다시 로그인하기</a>");
                 res.end();
             }
-        });
-    }else{
-        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-        res.write('<h2>데이터베이스 연결 실패</h2>');
-        res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
-        res.end();
-    }
-    
+        })
+    } 
 });
 
 // 사용자 추가 라우팅 함수
